3f035e5360d7aa31fee9393587e6a3255a392187
jlb <jlb@localhost>
2017-02-28 12:37:37 +0100

    popeel.py with minimal testing

--- popeel.py
+++ popeel.py
@@ -25,9 +25,8 @@ class Popeel:
 			self.basket = 0
 		else:
 			self.basket = randrange(1, 16)
-		
 
-	def task(n):
+	def set_task(self, n):
 		"set task to peeling n potatoes"
 		self.task = n
 	
@@ -37,11 +36,11 @@ class Popeel:
 		if self.basket > 0:
 			self.potatoes += 1
 			self.basket -= 1
+			print _create_message("Peeling one potato.", mm)
 			if (self.task - self.potatoes) % 10 == 0:
 				mm = "I have " + str(self.potatoes) + " peeled potatoes now."
 			else:
 				mm = ""
-			print _create_message("Peeling one potato.", mm)
 			if self.basket == self.task:
 				print _create_message("I have completed my task.",
 									"I LIKE that very much. :-)")
@@ -55,6 +54,7 @@ class Popeel:
 		if self.basket > 0:
 			print _create_message("I HATE to be sent for potatoes when I still have " + str(self.basket) + " left. :-(")
 		else:
+			print _create_message("I don't have any potatoes to peel, going for more of them.")
 			self.basket = randrange(5, 30)
 			print _create_message("Fetched more potatoes.", "I have now " + str(self.basket) + " of them. :-)")
 			
@@ -65,12 +65,30 @@ class Popeel:
 	def discard_basket(self):
 		if self.sleeping:
 			return
-		print _create_message("Returning " + str(self.basket) + " potatoes to the main store. :-)")
+		if self.basket > 0:
+			print _create_message("Returning " + str(self.basket) + " potatoes to the main store. :-)")
 		self.basket = 0
 	
 	def basket_is_empty(self):
 		if self.sleeping:
+			print _create_message("I am sleeping: don't ask me.")
 			return
-		return 
+		return self.basket == 0
 
+	def enough_potatoes(self):
+		if self.sleeping:
+			print _create_message("I am sleeping: don't ask me.")
+			return
+		return self.potatoes == self.task
+
+if __name__ == "__main__":
+	
+	p = Popeel()
+	p.set_task(23)
+	while not p.enough_potatoes():
+		if p.basket_is_empty():
+			p.refill_basket()
+		p.peel_1_potato()
+	p.discard_basket()
+	p.go_sleep()
 
